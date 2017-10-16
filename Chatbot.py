a3067247046a905ca54ece595f6cd2b09d61ac5b
Gaurav Sahadev <gauravsahadeo6@gmail.com>
2017-10-16 14:56:47 +0530

    Chatbot.py

---
+++ Chatbot.py
@@ -0,0 +1,40 @@
+
+from chatterbot import ChatBot
+
+# Uncomment the following lines to enable verbose logging
+# import logging
+# logging.basicConfig(level=logging.INFO)
+
+# Create a new instance of a ChatBot
+bot = ChatBot(
+    "Friday",
+    storage_adapter="chatterbot.storage.SQLStorageAdapter",
+    logic_adapters=[
+        "chatterbot.logic.MathematicalEvaluation",
+        "chatterbot.logic.TimeLogicAdapter",
+        "chatterbot.logic.BestMatch"
+    ],
+    input_adapter="chatterbot.input.TerminalAdapter",
+    output_adapter="chatterbot.output.TerminalAdapter",
+    database="../database2.db"
+)
+
+
+print("Type Something to Begin..")
+
+while True:
+	try:
+		bot_input=bot.get_response(None)
+
+	except(KeyboardInterrupt,EOFError,SystemExit):
+		break
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+chatterbot = ChatBot("Training Example")
+chatterbot.set_trainer(ChatterBotCorpusTrainer)
+
+chatterbot.train(
+    "chatterbot.corpus.english",
+    "chatterbot.corpus.english"
+)
+
---
+++ README.md
@@ -0,0 +1,5 @@
+create simple chatbot using chatterbot machine learning dialouge engine.
+the repository contains a sql database file which will be updated whenever 
+we start a conversation with the chatbot.
+Currently the chatbot is in learning process.
+
